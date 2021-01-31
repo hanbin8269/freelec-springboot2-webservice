078ddfe1cc21eb532e75cac9704c9c8bbfe61763
hanbin8269 <hanbin8269@gmail.com>
2021-01-31 20:08:30 +0900

    엔티티 생성 시간을 기록하기 위해 JPA Auditing 활성화

--- src/main/java/com/hanbin/book/springboot/Application.java
+++ src/main/java/com/hanbin/book/springboot/Application.java
@@ -1,7 +1,9 @@
 package com.hanbin.book.springboot;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
+@EnableJpaAuditing // JPA Auditing 활성화
 @SpringBootApplication // SpringBootApplication이 있는 위치부터 Bean 읽기나 생성을 시작함
 public class Application {
     public static void main(String[] args) {
