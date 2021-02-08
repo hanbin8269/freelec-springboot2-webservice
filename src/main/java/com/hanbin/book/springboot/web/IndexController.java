85f54b883c2c1cba2dc8aaa6161bf88bd3faf2b2
hanbin8269 <hanbin8269@gmail.com>
2021-02-08 23:20:51 +0900

    LoginUser 어노테이션 사용

--- src/main/java/com/hanbin/book/springboot/web/IndexController.java
+++ src/main/java/com/hanbin/book/springboot/web/IndexController.java
@@ -1,6 +1,8 @@
 package com.hanbin.book.springboot.web;
 
 
+import com.hanbin.book.springboot.config.auth.LoginUser;
+import com.hanbin.book.springboot.config.auth.dto.SessionUser;
 import com.hanbin.book.springboot.service.posts.PostsService;
 import com.hanbin.book.springboot.web.dto.PostsResponseDto;
 import lombok.RequiredArgsConstructor;
@@ -8,7 +10,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
+
 
 @RequiredArgsConstructor
 @Controller
@@ -17,8 +19,11 @@ public class IndexController {
     private final PostsService postsService;
 
     @GetMapping("/")
-    public String index(Model model){
+    public String index(Model model, @LoginUser SessionUser user){
         model.addAttribute("posts", postsService.findAllDesc()); // postsService.findAllDesc() 에서 가져온 결과는 posts로 index.mustache에 전달함
+        if (user != null){
+            model.addAttribute("userName", user.getName());
+        }
         return "index";
     }
 
