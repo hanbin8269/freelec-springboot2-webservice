e58559d4e14ec1158b1f7b02b9424437198b577e
hanbin8269 <hanbin8269@gmail.com>
2021-02-09 01:11:03 +0900

    WebMvcTest가 WebSecurityConfigurerAdapter(SecurityConfig)는 읽지만 의존성으로 가지고 있는 @Service(CustomOAuth2UserService)는 읽지 못하기 때문에 오류가 발생한다. 이를 막기 위해 SecurityConfig 제외 처리

--- src/test/java/com/hanbin/book/springboot/web/HelloControllerTest.java
+++ src/test/java/com/hanbin/book/springboot/web/HelloControllerTest.java
@@ -1,8 +1,12 @@
 package com.hanbin.book.springboot.web;
+import com.hanbin.book.springboot.config.auth.SecurityConfig;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.FilterType;
+import org.springframework.security.test.context.support.WithMockUser;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.ResultActions;
@@ -12,12 +16,16 @@ import static org.springframework.test.web.servlet.request.MockMvcRequestBuilder
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
 
 @RunWith(SpringRunner.class) // 테스트를 진행할 때 JUnit에 내장된 실행자 외에 다른 실행자를 실행시킨다
-@WebMvcTest(controllers=HelloController.class) // Web(Spring MVC)에 집중할 수 있는 어노테이션
+@WebMvcTest(controllers=HelloController.class,
+    excludeFilters = {
+        @ComponentScan.Filter(type = FilterType.ASSIGNABLE_TYPE, classes = SecurityConfig.class)
+    }) // Web(Spring MVC)에 집중할 수 있는 어노테이션
 public class HelloControllerTest {
 
     @Autowired // 스프링이 관리하는 Bean을 주입받는다
     private MockMvc mvc; // 스프링 MVC 테스트의 시작점이다
 
+    @WithMockUser(roles = "USER")
     @Test
     public void hello가_리턴된다() throws Exception {
         String hello = "hello";
@@ -29,6 +37,7 @@ public class HelloControllerTest {
                 .andExpect(content().string(hello)); // Controller에서 hello를 리턴하기 떄문에 값이 맞는지 검증
     }
 
+    @WithMockUser(roles = "USER")
     @Test
     public void helloDto가_리턴된다() throws Exception {
         String name = "hello";
