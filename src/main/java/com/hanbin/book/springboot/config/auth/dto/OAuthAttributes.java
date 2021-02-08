14abe00979965754037f46d41e01ff60bb3fa43c
hanbin8269 <hanbin8269@gmail.com>
2021-02-08 23:19:03 +0900

    네이버 OAuth 생성

--- src/main/java/com/hanbin/book/springboot/config/auth/dto/OAuthAttributes.java
+++ src/main/java/com/hanbin/book/springboot/config/auth/dto/OAuthAttributes.java
@@ -24,12 +24,15 @@ public class OAuthAttributes {
         this.picture = picture;
     }
 
-    // OAuth2User 에서 반환하는 사용자 정보는 Map이기 떄문에 값 하나하나를 반환해야만 함
-    public static OAuthAttributes of(String registrationId, String userNameAttributeName, Map<String, Object> attributes){
-        return ofGoogle(userNameAttributeName,attributes);
+    public static OAuthAttributes of(String registrationId, String userNameAttributeName, Map<String, Object> attributes) {
+        if("naver".equals(registrationId)) {
+            return ofNaver("id", attributes);
+        }
+
+        return ofGoogle(userNameAttributeName, attributes);
     }
 
-    private static OAuthAttributes ofGoogle(String userNameAttributeName, Map<String, Object> attributes){
+    private static OAuthAttributes ofGoogle(String userNameAttributeName, Map<String, Object> attributes) {
         return OAuthAttributes.builder()
                 .name((String) attributes.get("name"))
                 .email((String) attributes.get("email"))
@@ -39,7 +42,19 @@ public class OAuthAttributes {
                 .build();
     }
 
-    public User toEntity(){
+    private static OAuthAttributes ofNaver(String userNameAttributeName, Map<String, Object> attributes) {
+        Map<String, Object> response = (Map<String, Object>) attributes.get("response");
+
+        return OAuthAttributes.builder()
+                .name((String) response.get("name"))
+                .email((String) response.get("email"))
+                .picture((String) response.get("profile_image"))
+                .attributes(response)
+                .nameAttributeKey(userNameAttributeName)
+                .build();
+    }
+
+    public User toEntity() {
         return User.builder()
                 .name(name)
                 .email(email)
