acc88dde7cd05b2bcd2b9cb001ef8a367dce9a2e
hanbin8269 <hanbin8269@gmail.com>
2021-02-09 01:09:07 +0900

    @SpringBootApplication 과 @EnableJpaAuditing을 같이 두면 WebMockTest가 둘 다 읽어버리기 때문에 분리

--- src/main/java/com/hanbin/book/springboot/Application.java
+++ src/main/java/com/hanbin/book/springboot/Application.java
@@ -3,7 +3,6 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
-@EnableJpaAuditing // JPA Auditing 활성화
 @SpringBootApplication // SpringBootApplication이 있는 위치부터 Bean 읽기나 생성을 시작함
 public class Application {
     public static void main(String[] args) {
