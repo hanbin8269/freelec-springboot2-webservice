0bd791beb5369b83313646e861aa6d07233c732a
hanbin8269 <hanbin8269@gmail.com>
2021-02-18 09:36:06 +0900

    오타 수정

--- scripts/start.sh
+++ scripts/start.sh
@@ -1,4 +1,5 @@
 #!/usr/bin/env bash
+
 ABSPATH=$(readlink -f $0)
 ABSDIR=$(dirname $ABSPATH)
 source ${ABSDIR}/profile.sh
@@ -11,20 +12,21 @@ echo "> cp $REPOSITORY/zip/*.jar $REPOSITORY/"
 
 cp $REPOSITORY/zip/*.jar $REPOSITORY/
 
-echo "> 새 애플리케이션 배포"
+echo "> 새 어플리케이션 배포"
 JAR_NAME=$(ls -tr $REPOSITORY/*.jar | tail -n 1)
 
-echo "> JAR Name : $JAR_NAME"
+echo "> JAR Name: $JAR_NAME"
 
-echo "> $JAR_NAME 에 실행 권한 추가"
+echo "> $JAR_NAME 에 실행권한 추가"
+
 chmod +x $JAR_NAME
 
 echo "> $JAR_NAME 실행"
 
 IDLE_PROFILE=$(find_idle_profile)
 
-echo "> $JAR_NAME 를 profile=$IDLE_PROFILE 로 실행합니다"
+echo "> $JAR_NAME 를 profile=$IDLE_PROFILE 로 실행합니다."
 nohup java -jar \
-  -Dspring.config.location=classpath:/application.properties,classpath:/application-$IDLE_PROFILE.properties,/home/ec2-user/app/application-oauth.properties,/home/ec2-user/app/application-real-db.properties \
-  -Dspring.profiles.active=$IDLE_PROFILE \
-  $JAR_NAME > $JAR_NAME > $REPOSITORY/nohup.out 2>&1 &
+    -Dspring.config.location=classpath:/application.properties,classpath:/application-$IDLE_PROFILE.properties,/home/ec2-user/app/application-oauth.properties,/home/ec2-user/app/application-real-db.properties \
+    -Dspring.profiles.active=$IDLE_PROFILE \
+    $JAR_NAME > $REPOSITORY/nohup.out 2>&1 &
