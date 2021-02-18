1a8308afa6aa042b7193a8595d2014291afc0e2b
hanbin8269 <hanbin8269@gmail.com>
2021-02-18 09:43:55 +0900

    마지막 오타 수정

--- scripts/switch.sh
+++ scripts/switch.sh
@@ -9,7 +9,7 @@ function switch_proxy() {
 
     echo "> 전환할 port : $IDLE_PORT"
     echo "> Port 전환"
-    echo "set \$service_url http://127.0.0.1:$(IDLE_PORT);" | sudo tee /etc/nginx/conf.d/service-url.inc
+    echo "set \$service_url http://127.0.0.1:${IDLE_PORT};" | sudo tee /etc/nginx/conf.d/service-url.inc
 
     echo "> nginx Reload"
     sudo service nginx reload
